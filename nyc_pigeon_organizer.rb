b5fe3f19951c2d5b657a47922ebaaf358540ef6a
epang1 <epang1@gmail.com>
2016-02-06 19:12:27 -0500

    done

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+require 'pry'
+def nyc_pigeon_organizer(pigeon_data)
+new_hash = {}
+  pigeon_data.each do |attribute, colour|
+    colour.each do |colour, names|
+      names.each do |name|
+        if new_hash[name] == nil
+          new_hash[name] = {}
+        end
+        if new_hash[name][attribute] == nil
+          new_hash[name][attribute] = []
+        end
+        new_hash[name][attribute] << colour.to_s
+      end
+    end
+  end
+  return new_hash
 end
